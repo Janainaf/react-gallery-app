c99e38bd90d489fe1a476faa4c1772b586fa4b53
Janainaf <figueira.janaina@gmail.com>
2022-02-04 22:22:56 +0100

    refactoring and using hooks

--- src/App.js
+++ src/App.js
@@ -1,84 +1,73 @@
-import React from "react";
 import apiKey from "./config.js";
 import Photo from "./Photo";
 import SearchForm from "./SearchForm";
 import NavBar from "./NavBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, withRouter } from "react-router-dom";
 
 const api = apiKey;
 
-export class App extends React.Component {
-  state = {
-    photos: [],
-    name: "moon",
-  };
+function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("cats");
+  const [isLoading, setIsLoading] = useState(true);
+  const [sunsets, setSunsets] = useState([]);
+  const [beaches, setBeaches] = useState([]);
+  const [forests, setForests] = useState([]);
 
-  componentDidMount() {
-    window.addEventListener("load", this.handleClick("moon"));
-  }
+  const changeName = (value) => setQuery(value);
 
-  changeName = (query) => {
-    this.setState(
-      {
-        name: query,
-      },
-      this.handleClick
-    );
-  };
+  useEffect(() => {
+    fetch(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${query}&per_page=15&format=json&nojsoncallback=1&`
+    )
+      .then((response) => setData(response.data.data))
+      .catch((error) => console.log("Error fetching and parsing data", error))
+      .finally(() => setIsLoading(false));
+  }, [query]);
 
-  // componentDidMount() {
-  //   fetch(
-  //     `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${this.state.name}&per_page=15&format=json&nojsoncallback=1&`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((responseData) => {
-  //       this.setState({
-  //         name: this.state.name,
-  //         photos: responseData.photos.photo,
-  //       });
-  //     });
-  // }
+  useEffect(() => {
+    fetch(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=sunsets&per_page=15&format=json&nojsoncallback=1&`
+    )
+      .then((response) => setData(response.data.data))
+      .catch((error) => console.log("Error fetching and parsing data", error))
+      .finally(() => setIsLoading(false));
+  }, [sunsets]);
 
-  handleClick = async (name) => {
-    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${this.state.name}&per_page=15&format=json&nojsoncallback=1&`;
-    const response = await fetch(url);
-    const responseJson = await response.json();
-    const photo = await responseJson.photos;
-    const photos = await photo.photo;
-    this.setState({
-      photos: photos,
-      name: this.state.name,
-    });
-  };
+  useEffect(() => {
+    fetch(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=beaches&per_page=15&format=json&nojsoncallback=1&`
+    )
+      .then((response) => setData(response.data.data))
+      .catch((error) => console.log("Error fetching and parsing data", error))
+      .finally(() => setIsLoading(false));
+  }, [beaches]);
 
-  render() {
-    return (
+  useEffect(() => {
+    fetch(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=forests&per_page=15&format=json&nojsoncallback=1&`
+    )
+      .then((response) => setData(response.data.data))
+      .catch((error) => console.log("Error fetching and parsing data", error))
+      .finally(() => setIsLoading(false));
+  }, [forests]);
+
+  return (
+    <BrowserRouter>
       <div>
-        <SearchForm onSearch={this.changeName.bind(this)} />
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<NavBar />} />{" "}
-            <Route
-              exact
-              path="/sunsets"
-              element={<NavBar changeName={this.changeName.bind(this)} />}
-            />
-            <Route
-              exact
-              path="/beaches"
-              element={<NavBar changeName={this.changeName.bind(this)} />}
-            />
-            <Route
-              exact
-              path="/forests"
-              element={<NavBar changeName={this.changeName.bind(this)} />}
-            />{" "}
-          </Routes>
-        </Router>
-        {/* {this.state.photos && this.state.name && ( */}
-        <Photo photos={this.state.photos} />
+        <SearchForm onSearch={changeName} />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Photo photos={data} />} />
+          <Route path="/sunsets" element={<Photo photos={sunsets} />} />
+          <Route path="/beaches" element={<Photo photos={beaches} />} />
+          <Route path="/forests" element={<Photo photos={forests} />} />
+        </Routes>
       </div>
-    );
-  }
+      {isLoading ? <p>Loading...</p> : <Photo data={data} />}
+    </BrowserRouter>
+  );
 }
+
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,29 @@
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function NavbBar(props) {
+  const navigate = useNavigate();
+
   return (
     <nav className="main-nav">
       <ul>
         <li>
-          <Link to="/sunsets" onClick={() => props.changeName("sunsets")}>
-            {" "}
+          <Link
+            to="/sunsets"
+            onClick={() => {
+              navigate("/sunsets");
+            }}
+          >
             Sunsets{" "}
           </Link>
         </li>
         <li>
-          <Link to="/beaches" onClick={() => props.changeName("beaches")}>
+          <Link to="/beaches" onClick={() => navigate("/beaches")}>
             Beaches{" "}
           </Link>
         </li>
         <li>
-          <Link to="/forests" onClick={() => props.changeName("forests")}>
+          <Link to="/forests" onClick={() => navigate("/forests")}>
             {" "}
             Forests{" "}
           </Link>
