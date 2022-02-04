d2f3205a05269b10e3492dc12532623476adafc9
Janainaf <figueira.janaina@gmail.com>
2022-02-04 22:53:17 +0100

    refactoring but there is a leak to be fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchForm from "./SearchForm";
 import NavBar from "./NavBar";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, withRouter } from "react-router-dom";
+import axios from "axios";
 
 const api = apiKey;
 
@@ -18,54 +19,54 @@ function App() {
   const changeName = (value) => setQuery(value);
 
   useEffect(() => {
-    fetch(
+    axios(
       `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${query}&per_page=15&format=json&nojsoncallback=1&`
     )
-      .then((response) => setData(response.data.data))
+      .then((response) => setData(response.data.photos.photo))
       .catch((error) => console.log("Error fetching and parsing data", error))
       .finally(() => setIsLoading(false));
   }, [query]);
 
-  useEffect(() => {
-    fetch(
-      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=sunsets&per_page=15&format=json&nojsoncallback=1&`
-    )
-      .then((response) => setData(response.data.data))
-      .catch((error) => console.log("Error fetching and parsing data", error))
-      .finally(() => setIsLoading(false));
-  }, [sunsets]);
+  // useEffect(() => {
+  //   axios(
+  //     `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=sunsets&per_page=15&format=json&nojsoncallback=1&`
+  //   )
+  //     .then((response) => setSunsets(response.data.photos.photo))
+  //     .catch((error) => console.log("Error fetching and parsing data", error))
+  //     .finally(() => setIsLoading(false));
+  // }, [sunsets]);
 
-  useEffect(() => {
-    fetch(
-      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=beaches&per_page=15&format=json&nojsoncallback=1&`
-    )
-      .then((response) => setData(response.data.data))
-      .catch((error) => console.log("Error fetching and parsing data", error))
-      .finally(() => setIsLoading(false));
-  }, [beaches]);
+  // useEffect(() => {
+  //   axios(
+  //     `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=beaches&per_page=15&format=json&nojsoncallback=1&`
+  //   )
+  //     .then((response) => setBeaches(response.data.photos.photo))
+  //     .catch((error) => console.log("Error fetching and parsing data", error))
+  //     .finally(() => setIsLoading(false));
+  // }, [beaches]);
 
-  useEffect(() => {
-    fetch(
-      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=forests&per_page=15&format=json&nojsoncallback=1&`
-    )
-      .then((response) => setData(response.data.data))
-      .catch((error) => console.log("Error fetching and parsing data", error))
-      .finally(() => setIsLoading(false));
-  }, [forests]);
-
+  // useEffect(() => {
+  //   axios(
+  //     `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=forests&per_page=15&format=json&nojsoncallback=1&`
+  //   )
+  //     .then((response) => setForests(response.data.photos.photo))
+  //     .catch((error) => console.log("Error fetching and parsing data", error))
+  //     .finally(() => setIsLoading(false));
+  // }, [forests]);
+  console.log(query);
   return (
     <BrowserRouter>
       <div>
         <SearchForm onSearch={changeName} />
         <NavBar />
         <Routes>
-          <Route path="/" element={<Photo photos={data} />} />
-          <Route path="/sunsets" element={<Photo photos={sunsets} />} />
+          <Route path="/:query" element={<Photo photos={data} />} />
+          {/* <Route path="/sunsets" element={<Photo photos={sunsets} />} />
           <Route path="/beaches" element={<Photo photos={beaches} />} />
-          <Route path="/forests" element={<Photo photos={forests} />} />
+          <Route path="/forests" element={<Photo photos={forests} />} /> */}
         </Routes>
       </div>
-      {isLoading ? <p>Loading...</p> : <Photo data={data} />}
+      {/* {isLoading ? <p>Loading...</p> : <Photo data={data} />} */}
     </BrowserRouter>
   );
 }
