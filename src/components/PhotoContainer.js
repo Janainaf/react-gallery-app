9eff78932ea59878eb279fcd1048f9068b5e92e0
Janainaf <figueira.janaina@gmail.com>
2022-02-07 16:59:52 +0100

    final commit

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,19 +1,24 @@
 import NotFound from "./NotFound";
 import Photo from "./Photo";
+import { useParams, useLocation } from "react-router-dom";
 
 function PhotoContainer(props) {
+  const params = useParams();
+  const location = useLocation();
+
   return (
     <>
       <div className="photo-container">
-        {props.photos.length > 0 ? (
+        {props.photos.length > 0 && params ? (
           <>
-            <h2>Results</h2>
+            <h2>Results {params.tag || location.pathname.replace(/\//, "")}</h2>
             <Photo photos={props.photos} />
           </>
         ) : (
           <NotFound />
         )}
         <Photo photos={props.photos} />
+        {/* {console.log(location)} */}
       </div>
     </>
   );
